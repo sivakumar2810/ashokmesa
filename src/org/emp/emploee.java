ba55e2ca84a2387beba4f9f2e4d9ca36f3e90268
sivakumar2810 <ashokmesa07@gmail.com>
2022-02-03 18:32:19 +0530

    hi am new

---
+++ src/org/emp/emploee.java
@@ -0,0 +1,46 @@
+package org.emp;
+
+public class emploee {
+	
+
+	int a;
+	
+	
+	public static void main(String[] args) {
+		emploee em = new emploee();
+		System.out.println("sleep");
+		
+	}
+	
+	
+	
+	
+	/*private void empid() {
+		System.out.println("b112ash");
+	}
+
+	private void empname() {
+		System.out.println("sivakumar A");
+	}
+
+	private void empdob() {
+		System.out.println("28101993");
+	}
+
+	private void empphone() {
+		System.out.println("9698513002");
+	}
+
+	private void empmail() {
+		System.out.println("ashokmesa07@gmail.com");
+	}
+
+	public static void main(String[] args) {
+		emploee details = new emploee();
+		details.empid();
+		details.empname();
+		details.empdob();
+		details.empphone();
+		details.empmail();*/
+}
+	
